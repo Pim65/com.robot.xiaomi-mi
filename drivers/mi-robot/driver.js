c7d87ea8a2c459ed17424dd157c154398d7cb38e
Jelger Haanstra <Jelger Haanstra>
2017-01-12 22:47:53 +0100

    Initial commit

--- drivers/mi-robot/driver.js
+++ drivers/mi-robot/driver.js
@@ -7,7 +7,7 @@ module.exports.init = function( devices_data, callback ) {
 
     devices_data.forEach(function initdevice(device) {
 
-	    devices[device.id] = device;
+        devices[device.id] = device;
 
 	    module.exports.getSettings(device, function(err, settings) {
 		    devices[device.id].settings = settings;
@@ -15,42 +15,12 @@ module.exports.init = function( devices_data, callback ) {
 
 	});
 
-    Homey.log ("Xiaomi Mi Robot - init done")
+    Homey.log ("Xiaomi Mi Robot - Init done");
 
 	callback (null, true);
 }
 
 module.exports.pair = function( socket ) {
-    socket.on('list_devices', function( data, callback ) {
-
-        var device_data = {
-            name: "Mi Robot",
-            data: {
-                address : data.address,
-                start   : data.start,
-                pause   : data.pause,
-                home    : data.home,
-                find    : data.find
-            }
-        }
-
-        callback( null, [ device_data ] );
-
-    });
-
-    socket.on('get_devices', function (data, callback) {
-
-        var address = data.address;
-        var start   = data.start;
-        var pause   = data.pause;
-        var home    = data.home;
-        var find    = data.find;
-
-		Homey.log ( "Xiaomi Mi Robot - got get_devices from front-end with address" + address );
-
-		socket.emit ('continue', null);
-
-	});
 
     socket.on('disconnect', function() {
         Homey.log ("User aborted pairing, or pairing is finished");
@@ -58,9 +28,9 @@ module.exports.pair = function( socket ) {
 
     socket.on('test-connection', function( data, callback ) {
         var address = data.address;
-        var pause   = data.pause;
+        var find   = data.find;
 
-        sendCommand(pause, address, callback);
+        sendCommand(find, address, callback);
 
         callback( null, true );
     });
@@ -85,23 +55,6 @@ module.exports.settings = function( device_data, newSettingsObj, oldSettingsObj,
     }
 }
 
-// FLOW ACTION HANDLERS
-Homey.manager('flow').on('action.startCleaning', function( callback, args ) {
-    sendCommand(devices[args.device.id].settings.start, devices[args.device.id].settings.address, callback);
-});
-
-Homey.manager('flow').on('action.stopCleaning', function( callback, args ) {
-    sendCommand(devices[args.device.id].settings.pause, devices[args.device.id].settings.address, callback);
-});
-
-Homey.manager('flow').on('action.startCleaning', function( callback, args ) {
-    sendCommand(devices[args.device.id].settings.home, devices[args.device.id].settings.address, callback);
-});
-
-Homey.manager('flow').on('action.startCleaning', function( callback, args ) {
-    sendCommand(devices[args.device.id].settings.find, devices[args.device.id].settings.address, callback);
-});
-
 // FUNCTIONS
 function sendCommand (command, address, callback) {
     var PORT = 54321;
@@ -114,4 +67,23 @@ function sendCommand (command, address, callback) {
         client.close();
     });
 
+    callback(null, true)
+
 }
+
+// FLOW ACTION HANDLERS
+Homey.manager('flow').on('action.startCleaning', function( callback, args ) {
+    sendCommand(devices[args.device.id].settings.start, devices[args.device.id].settings.address, callback);
+});
+
+Homey.manager('flow').on('action.stopCleaning', function( callback, args ) {
+    sendCommand(devices[args.device.id].settings.pause, devices[args.device.id].settings.address, callback);
+});
+
+Homey.manager('flow').on('action.goHomeMiRobot', function( callback, args ) {
+    sendCommand(devices[args.device.id].settings.home, devices[args.device.id].settings.address, callback);
+});
+
+Homey.manager('flow').on('action.findMiRobot', function( callback, args ) {
+    sendCommand(devices[args.device.id].settings.find, devices[args.device.id].settings.address, callback);
+});
